1b2d9388a0a1c23307bd4ebcc26fedcb38d627fa
jonoaugustine <jonoaugustine@jhu.edu>
2020-02-18 04:28:51 -0500

    update ignore & static host

--- src/middleware/token.js
+++ src/middleware/token.js
@@ -1,1 +1,20 @@
+
+
+class Token {
+  constructor({ uid, email }, raw) {
+    this.uid = uid;
+    this.email = email;
+    this.raw = raw;
+  }
+}
+
+/**
+ * Attempts to extract the Firebase Auth token from the request header
+ * and add the data to the req object as "token"
+ * @param {*} req
+ * @param {*} res
+ * @param {function} next
+ */
+const pullToken = (req, res, next) => {};
+
 module.exports = {};
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const { join } = require("path");
 
 const Server = async () => {
   const svr = express();
 
   svr.use(cors());
   svr.use(express.json());
+
+  // Static files
+  svr.use(express.static(join(__dirname, "..", "site", "dist")));
 
   // API docs
   svr.use("/api/docs", routes.docs);
