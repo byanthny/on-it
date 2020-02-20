1a4cdc660134bdd1d1d00ab66041a99fb58c68af
jonoaugustine <jonoaugustine@jhu.edu>
2020-02-20 04:00:48 -0500

    remove env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY)),
   databaseURL: "https://on-it-app.firebaseio.com"
 });
 
