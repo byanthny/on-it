caca5de17c88206e13b819e6cf61b3f6a22ce734
jonoaugustine <jonoaugustine@jhu.edu>
2020-03-01 18:53:48 -0500

    added error handling to create function

--- site/src/React.js
+++ site/src/React.js
@@ -18,29 +18,37 @@ window["React"] = {
     var element = document.createElement(tag);
 
     for (let name in attrs) {
-      if (name && attrs.hasOwnProperty(name)) {
-        let value = attrs[name];
-        if (value === true) {
-          element.setAttribute(name, name);
-        } else if (value !== false && value != null) {
-          if (typeof value === "function") {
-            element[name.toLowerCase()] = value;
-          } else if (name === "style") {
-            element.setAttribute(name, readStyle(value));
-          } else {
-            element.setAttribute(name, value.toString());
+      try {
+        if (name && attrs.hasOwnProperty(name)) {
+          let value = attrs[name];
+          if (value === true) {
+            element.setAttribute(name, name);
+          } else if (value !== false && value != null) {
+            if (typeof value === "function") {
+              element[name.toLowerCase()] = value;
+            } else if (name === "style") {
+              element.setAttribute(name, readStyle(value));
+            } else {
+              element.setAttribute(name, value.toString());
+            }
           }
         }
+      } catch (error) {
+        console.log(error);
       }
     }
 
     for (let i = 2; i < arguments.length; i++) {
-      let child = arguments[i];
-      element.appendChild(
-        child.nodeType == null
-          ? document.createTextNode(child.toString())
-          : child
-      );
+      try {
+        let child = arguments[i];
+        element.appendChild(
+          child.nodeType == null
+            ? document.createTextNode(child.toString())
+            : child
+        );
+      } catch (error) {
+        console.log(error);
+      }
     }
     return element;
   }
