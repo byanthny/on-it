d3b87f064a464fa6c8e9fcbeabf7d9dfa6f75481
Jono <jaugust8@jhu.edu>
2020-04-03 00:37:32 -0400

    format

--- site/src/pages/DesktopApp.jsx
+++ site/src/pages/DesktopApp.jsx
@@ -1,90 +1,96 @@
 import { API, Task } from "../api";
 
+const DesktopApp = () => {
+  const root = document.getElementById("root");
+  var setBackground = hex => {
+    root.style.backgroundColor = hex;
+  };
 
-const DesktopApp = ()  => {
-    const root = document.getElementById("root");
-    var setBackground = (hex) => {
-        root.style.backgroundColor = hex;
-    }
+  //TODO Date update on new day
+  /* Gets current date on "MAR. 4" format*/
+  var date = () => {
+    var dt = new Date();
+    Date.shortMonths = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    return Date.shortMonths[dt.getMonth()].toUpperCase() + ". " + dt.getDay();
+  };
 
-    //TODO Date update on new day
-    /* Gets current date on "MAR. 4" format*/
-    var date = () => {
-        var dt = new Date();
-        Date.shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        return Date.shortMonths[dt.getMonth()].toUpperCase() + ". " + dt.getDay();
-    }
+  var projects = API.projects.getAll();
 
-    var projects = API.projects.getAll();
-
-    var createProject = (p) => {
-        //<a class="project-name current"><h5>{p}</h5></a>
-        return (
-            <a class="project-name"><h5>{p}</h5></a>
-        );
-    }
-
-
-
+  var createProject = p => {
+    //<a class="project-name current"><h5>{p}</h5></a>
     return (
-        <div class="container">
-            <div id="settings">
+      <a class="project-name">
+        <h5>{p}</h5>
+      </a>
+    );
+  };
 
-            </div>
-            {/* TODO customizable background*/
-            setBackground("#242424")}
+  return (
+    <div class="container">
+      <div id="settings"></div>
+      {/* TODO customizable background*/
+      setBackground("#242424")}
 
-            <div class="flex-box-col">
-                <div id="header">
-                    <h4>{date()}</h4>
-                </div>
-                <div class="flex-box-row">
-                    <div id="projects">
-                        {/* TODO load in list of projects and  */}
-                        {createProject("🥴 Inbox")}
-                        {createProject("🔥 Today")}
+      <div class="flex-box-col">
+        <div id="header">
+          <h4>{date()}</h4>
+        </div>
+        <div class="flex-box-row">
+          <div id="projects">
+            {/* TODO load in list of projects and  */}
+            {createProject("🥴 Inbox")}
+            {createProject("🔥 Today")}
 
-                        <hr></hr>
-                        <h4>Projects</h4>
-                        <div class="usr-projects">
-                            {createProject("🔥 Test")}
+            <hr></hr>
+            <h4>Projects</h4>
+            <div class="usr-projects">
+              {createProject("🔥 Test")}
 
-                            {/*projects.array.forEach(element => {
+              {/*projects.array.forEach(element => {
                                 createProject(element.name);
                             })*/}
-
-                        </div>
-                    </div>
-
-                    <div id="data">
-                        <div id="task" class="view">
-                                <div id="projs">
-                                <button
-                                onClick={e => {
-                                e.preventDefault();
-                                //API.projects.create("TesT-prOject-name");
-                                //API.projects.delete("Project-Name-One").then(r => console.log(r));
-                                loadProjects();
-                                }}
-                                >
-                                Test
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-            
+          </div>
 
-            {/* TODO */}
-            <div class="help">
-                <a>?</a>
-                <div class="help-info">
-
-                </div>
+          <div id="data">
+            <div id="task" class="view">
+              <div id="projs">
+                <button
+                  onClick={e => {
+                    e.preventDefault();
+                    //API.projects.create("TesT-prOject-name");
+                    //API.projects.delete("Project-Name-One").then(r => console.log(r));
+                    loadProjects();
+                  }}
+                >
+                  Test
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-      );
-}
+      </div>
+
+      {/* TODO */}
+      <div class="help">
+        <a>?</a>
+        <div class="help-info"></div>
+      </div>
+    </div>
+  );
+};
 
 export default DesktopApp;
