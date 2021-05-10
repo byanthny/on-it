1d35eddbd7f1b028d1826cbb18692355326d392c
Jono <jaugust8@jhu.edu>
2021-05-10 18:52:43 -0400

    Common: re-export models from root index

---
+++ projects/common/src/index.ts
@@ -0,0 +1,25 @@
+import {
+  nameSchema,
+  User,
+  userSchema,
+  Project,
+  projectSchema,
+  Task,
+  taskSchema,
+  TaskState,
+  Note,
+  noteSchema,
+} from "./models"
+
+export {
+  nameSchema,
+  User,
+  userSchema,
+  Project,
+  projectSchema,
+  Task,
+  taskSchema,
+  TaskState,
+  Note,
+  noteSchema,
+}
