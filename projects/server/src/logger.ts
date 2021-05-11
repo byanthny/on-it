b86071c08e27193434077f9c0db15ac0348115d1
Jono <jaugust8@jhu.edu>
2021-05-11 01:38:05 -0400

    Server: improve logger

--- projects/server/src/logger.ts
+++ projects/server/src/logger.ts
@@ -3,5 +3,15 @@ import { transports, configure, format } from "winston"
 configure({
   level: process.env.STAGE === "PRODUCTION" ? "info" : "debug",
   transports: [new transports.Console()],
-  format: format.simple(),
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message, ...meta }) => {
+      let s = `${timestamp} [${level}]: ${message}`
+      if (Object.keys(meta).length > 0) {
+        s += `\n${JSON.stringify(meta, null, 2)}`
+      }
+      return s
+    }),
+  ),
 })
