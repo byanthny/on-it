9947fbea5919003fe4f3951ff4b13e6b64cd2986
Jono <jaugust8@jhu.edu>
2021-05-11 01:46:44 -0400

    Site: add login api call

--- projects/site/src/api/user.ts
+++ projects/site/src/api/user.ts
@@ -17,3 +17,21 @@ export const register = async (
 
   return payload
 }
+
+/** @param identity - display-name or email */
+export const login = async (
+  identity: string,
+  password: string,
+): Promise<{ user: User; token: string }> => {
+  const {
+    data: { payload, error },
+  } = await axios.post<ApiResponse<{ user: User; token: string }>>(
+    "/users/login",
+    { identity, password },
+  )
+
+  if (error) throw new Error(error)
+  else if (!payload) throw new Error("Failed to retrieve user data")
+
+  return payload
+}
