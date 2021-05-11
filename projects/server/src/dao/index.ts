7142ec9212267415c1e4de863a7e95e2fca1654c
Jono <jaugust8@jhu.edu>
2021-05-11 01:51:28 -0400

    Server: get current User dao function

--- projects/server/src/dao/index.ts
+++ projects/server/src/dao/index.ts
@@ -1,3 +1,20 @@
+import { User } from "common"
+import { Client, CurrentIdentity, Get } from "faunadb"
+import { Document } from "../types/fauna"
 import * as users from "./users"
 
-export default { users }
+/**
+ * Attemps to get the User document for the given token
+ */
+const identify = async (token: string): Promise<User> => {
+  const {
+    data,
+    ref: { id },
+  } = await new Client({ secret: token }).query<Document<User>>(
+    Get(CurrentIdentity()),
+  )
+
+  return { ...data, _id: id }
+}
+
+export default { users, identify }
