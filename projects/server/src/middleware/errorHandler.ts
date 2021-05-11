7b44aae7fad238706c77ab90f2f5066192b6431d
Jono <jaugust8@jhu.edu>
2021-05-11 01:39:53 -0400

    Server: handle unknown error resoponses

--- projects/server/src/middleware/errorHandler.ts
+++ projects/server/src/middleware/errorHandler.ts
@@ -1,12 +1,17 @@
 import { DuplicateError, MalformedContentError } from "../errors"
 import { Request, Response } from "../types/express"
+import logger from "winston"
 
 const errorHandler = (error: Error, _: Request, res: Response, __: any) => {
   let message: string = error.message ?? "Unknown error"
 
   if (error instanceof MalformedContentError) res.status(400)
   else if (error instanceof DuplicateError) res.status(409)
-  else res.status(500)
+  else if (error.message.match(/authentication failed/i)) res.status(401)
+  else {
+    logger.info("unknown error", { error })
+    res.status(500)
+  }
 
   res.error(message)
 }
