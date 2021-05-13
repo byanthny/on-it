810a7a4063184bb3c8ec344d88ff886ce66e3609
Jono <jaugust8@jhu.edu>
2021-05-13 00:11:47 -0400

    Server: project dao update and delete

--- projects/server/src/dao/projects.ts
+++ projects/server/src/dao/projects.ts
@@ -1,10 +1,8 @@
-import { Create, Get, Ref } from "faunadb"
+import { Create, Delete, Get, Ref, Update } from "faunadb"
 import { Document } from "../types/fauna"
-import { Project, User } from "common"
+import { Project } from "common"
 import db from "./root"
 import collections from "./collections"
-import indexes from "./indexes"
-import logger from "winston"
 
 export const create = async (
   uid: string,
@@ -27,3 +25,27 @@ export const getByID = async (pid: string): Promise<Project> => {
   } = await db.query<Document<Project>>(Get(Ref(collections.projects, pid)))
   return { ...data, _id: id }
 }
+
+export const update = async (
+  pid: string,
+  name: string,
+  color: string,
+): Promise<Project> => {
+  const {
+    data,
+    ref: { id },
+  } = await db.query<Document<Project>>(
+    Update(Ref(collections.projects, pid), { data: { name, color } }),
+  )
+  return { ...data, _id: id }
+}
+
+const _delete = async (pid: string): Promise<Project> => {
+  const {
+    data,
+    ref: { id },
+  } = await db.query<Document<Project>>(Delete(Ref(collections.projects, pid)))
+  return { ...data, _id: id }
+}
+
+export { _delete as delete }
