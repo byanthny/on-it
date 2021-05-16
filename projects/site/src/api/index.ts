9d1a640bea19fb6754428f3e33061b2fc84f8a59
Jono <jaugust8@jhu.edu>
2021-05-16 02:52:10 -0400

    Site: add task api calls

--- projects/site/src/api/index.ts
+++ projects/site/src/api/index.ts
@@ -1,4 +1,6 @@
 import * as user from "./user"
 import * as project from "./project"
+import * as task from "./task"
 
-export default { user, project }
+export { user, project, task }
+export default { user, project, task }
