221a3921f388853ea2132983ee2520472333c8c9
Jono <jaugust8@jhu.edu>
2021-05-17 00:30:45 -0400

    Site: search project call

--- projects/site/src/api/project.ts
+++ projects/site/src/api/project.ts
@@ -1,19 +1,21 @@
 import { apiRequest, axios } from "./axios"
-import { ID, Project } from "common"
+import { ID, Project, ProjectSearch } from "common"
 
 export const createProject = async (
   token: string,
   name: string,
   color?: string,
 ): Promise<Project> =>
-  apiRequest<Project>(
-    axios.post("/projects", { name, color }, { headers: { token } }),
-  )
+  apiRequest(axios.post("/projects", { name, color }, { headers: { token } }))
 
 export const getProject = async (token: string, pid: ID): Promise<Project> =>
-  apiRequest<Project>(axios.get(`projects/${pid}`, { headers: { token } }))
+  apiRequest(axios.get(`projects/${pid}`, { headers: { token } }))
 
-// TODO project search
+export const searchProject = async (
+  token: string,
+  search: ProjectSearch,
+): Promise<Project[]> =>
+  apiRequest(axios.get(`/projects`, { params: search, headers: { token } }))
 
 export const updateProject = async (
   token: string,
