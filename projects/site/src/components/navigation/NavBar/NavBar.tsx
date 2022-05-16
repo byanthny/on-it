868c29a91d52db0b4c56c94185bf53252e97e74a
Anthony D <adas22@jhu.edu>
2022-05-16 17:14:26 -0400

    frontend: navbar styled

--- projects/site/src/components/navigation/NavBar/NavBar.tsx
+++ projects/site/src/components/navigation/NavBar/NavBar.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { RiSettings3Fill, RiHomeFill, RiStickyNoteFill, RiCheckboxFill, RiAddFill } from "react-icons/ri";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => (
     <div className={styles.navbar}>
         <div className={styles.innerbox}>
-            <Link to="/">home</Link>
-            <Link to="/todo">todo</Link>
-            <button type="button">+</button>
-            <Link to="/notes">notes</Link>
-            <Link to="/settings">settings</Link>
+            <Link aria-label="home" to="/"><RiHomeFill /></Link>
+            <Link aria-label="to do" to="/todo"><RiCheckboxFill/></Link>
+            {/* eslint-disable-next-line */}
+            <button aria-label="add" type="button" onClick={()=>{console.log("clicked")}}><div className={styles.buttonwrapper}><div className={styles.buttonfixed}><RiAddFill/></div></div></button>
+            <Link aria-label="notes" to="/notes"><RiStickyNoteFill/></Link>
+            <Link aria-label="settings" to="/settings"><RiSettings3Fill /></Link>
         </div>
     </div>
 );
