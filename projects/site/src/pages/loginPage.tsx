974910902c2cc33d827d552f2a46bd1792cdbd9c
Anthony D <adas22@jhu.edu>
2022-06-09 20:04:36 -0400

    frontend: init login/signup page

--- projects/site/src/pages/loginPage.tsx
+++ projects/site/src/pages/loginPage.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from "react";
 
-const loginPage = () => <h1>Login Page!</h1>;
+const loginPage = () => {
+  const [login, setLogin] = useState(true);
+  return (
+    <div className="main-content">
+      <h1>Login</h1>
+      <form>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password" />
+          {!login ? (<input type="password" placeholder="password" />) : ("")}
+          <button type="submit" aria-label="submit form">{login ? "Login" : "Sign-Up"}</button>
+      </form>
+      <button type="button" onClick={()=>setLogin(!login)}>{login ? "Dont have an account?" : "Already have an account?"}</button>
+    </div>
+
+    );
+};
 
 export default loginPage;
