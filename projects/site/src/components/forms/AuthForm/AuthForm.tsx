441a8cbfa96363a4c2534d4ff6963c7011f48613
Anthony D <adas22@jhu.edu>
2022-06-10 00:17:37 -0400

    frontend: formatting + minor styling

--- projects/site/src/components/forms/AuthForm/AuthForm.tsx
+++ projects/site/src/components/forms/AuthForm/AuthForm.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import styles from "./authForm.module.scss";
 
 interface PropTypes {
-    loginState: boolean
+  loginState: boolean;
 }
 
 // TODO api + update usercontext
 
-const LoginForm = ({loginState}:PropTypes) => (
-
-    <form className={styles.authForm} >
-    { /* <h2>{loginState ? "Login" : "Sign Up"}</h2> */ }
-        <input type="email" placeholder="email"/>
-        <input type="password" placeholder="password" />
-        {!loginState ? (<input type="password" placeholder="password" />) : ("")}
-        <button type="submit" aria-label="submit form">{loginState ? "Login" : "Sign Up"}</button>
-    </form>
+const LoginForm = ({ loginState }: PropTypes) => (
+  <form className={styles.authForm}>
+    {/* <h2>{loginState ? "Login" : "Sign Up"}</h2> */}
+    <input type="email" placeholder="email" />
+    <input type="password" placeholder="password" />
+    {!loginState ? <input type="password" placeholder="password" /> : ""}
+    <button type="submit" aria-label="submit form">
+      {loginState ? "Login" : "Sign Up"}
+    </button>
+  </form>
 );
 
 export default LoginForm;
