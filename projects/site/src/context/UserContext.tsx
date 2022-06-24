8e3f44036c780a7f2245a985848f5d16d7afc3b2
Anthony D <adas22@jhu.edu>
2022-06-23 23:18:13 -0400

    frontend: UserContext

--- projects/site/src/context/UserContext.tsx
+++ projects/site/src/context/UserContext.tsx
@@ -1,1 +1,32 @@
-export {};
+/* eslint react/jsx-no-constructed-context-values: 0 */
+
+import React, { createContext, useState } from "react";
+
+interface User {
+    loggedIn: boolean
+    id: string,
+    email: string,
+    password: string
+}
+
+const initalUser: User = {
+    loggedIn: false,
+    id: "null",
+    email: "null",
+    password: "null"
+} 
+interface UserProviderProps {
+  children: React.ReactNode
+};
+
+export const UserContext = createContext<any>(initalUser);
+
+export const UserProvider = ({ children }:UserProviderProps) => {
+  const [user, setUser] = useState(initalUser);
+  const value = {user, setUser}
+  return (
+  <UserContext.Provider value={value}>
+    {children}
+    </UserContext.Provider>
+    );
+};
