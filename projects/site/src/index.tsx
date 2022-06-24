5139979413b1b7e1dae321b1cc411c739e924f85
Anthony D <adas22@jhu.edu>
2022-06-24 17:18:04 -0400

    frontend: forgot to add UserProvider

--- projects/site/src/index.tsx
+++ projects/site/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
+import { UserProvider } from "./context/UserContext";
 import "./styles/global.scss";
 import "./styles/typography.scss";
 
 ReactDOM.render(
   <ThemeProvider>
+    <UserProvider>
     <App />
+    </UserProvider>
   </ThemeProvider>,
   document.getElementById("root"),
 );
