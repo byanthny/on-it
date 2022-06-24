39f9525142ef9a289d83e4e6a6d4e2f828763fcd
Anthony D <adas22@jhu.edu>
2022-06-23 23:18:59 -0400

    frontend: basic user authentication

--- projects/site/src/components/forms/AuthForm/AuthForm.tsx
+++ projects/site/src/components/forms/AuthForm/AuthForm.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./authForm.module.scss";
 
 interface PropTypes {
   loginState: boolean;
+  submit: Function
 }
 
 // TODO api + update usercontext
 
-const LoginForm = ({ loginState }: PropTypes) => (
+const AuthForm = ({ loginState, submit }: PropTypes) => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submitForm = (e:string, p: string, cp:string) => {
+    if (!loginState && p !== cp) {
+      console.log("password don't match")
+      return
+    }
+    submit(e, p);
+  }
+  
+  return (
   <form className={`${styles.authForm} light`}>
     {/* <h2>{loginState ? "Login" : "Sign Up"}</h2> */}
-    <input type="email" placeholder="email" />
-    <input type="password" placeholder="password" />
-    {!loginState ? <input type="password" placeholder="confirm password" /> : ""}
-    <button type="submit" aria-label="submit form">
+    <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)}/>
+    <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+    {!loginState ? <input type="password" placeholder="confirm password" onChange={e=>setConfirmPassword(e.target.value)}/> : ""}
+    <button type="submit" aria-label="submit form" onClick={()=>submitForm(email, password, confirmPassword)}>
       {loginState ? "Login" : "Sign Up"}
     </button>
-  </form>
-);
+  </form>);
+};
 
-export default LoginForm;
+export default AuthForm;
--- projects/site/src/pages/authPage.tsx
+++ projects/site/src/pages/authPage.tsx
@@ -1,15 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthForm from "../components/forms/AuthForm/AuthForm";
+import {UserContext} from "../context/UserContext"
+import OnItApi from "../services/OnItApi";
 
 const authPage = () => {
   const [login, setLogin] = useState(true);
+  const {setUser} = useContext(UserContext);
+
+  const submitForm = async (email:string, password:string) => {
+    if(login) {
+      const response = await OnItApi.login( email, password);
+      console.log(response);
+    }
+    else {
+      const response = await OnItApi.register( email, password);
+      console.log(response);
+    }
+  }
+
   return (
     <>
       <div className="main-content">
         <div className="secondary-content-xs authPage">
           <h1>On-It</h1>
-          <AuthForm loginState={login} />
+          <AuthForm loginState={login} submit={submitForm} />
           <button className="buttonLink" type="button" onClick={() => setLogin(!login)}>
             {login ? "Dont have an account?" : "Already have an account?"}
           </button>
