92b8decdaaff03e1f06da725e2cb1d4022fb47bf
Anthony D <adas22@jhu.edu>
2022-06-26 02:15:53 -0400

    frontend: accordion for notes

--- projects/site/src/components/notes/NoteCollection/NoteCollection.tsx
+++ projects/site/src/components/notes/NoteCollection/NoteCollection.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiArrowDropDownLine, RiArrowDropRightLine } from "react-icons/ri"
+import Button from "../../interactive/Button/Button";
 import Note from "../Note/Note"
 import styles from "./noteCollection.module.scss"
 
@@ -12,11 +14,13 @@ const NoteCollection = ({data, collectionTitle}:PropTypes) => {
     const notesList = data.map(({ title, text, tags, updatedAt }) => (
         <Note title={title} text={text} tags={tags} updatedAt={updatedAt} />
       ));
+
+    const [open, setOpen] = useState(true);
         
       return (
         <div>
-            <h2>{collectionTitle}</h2>
-            <div className={styles.noteCollection}>
+            <h2>{collectionTitle}<Button variant="transparent" onClickFunction={()=>setOpen(!open)}>{open ? <RiArrowDropDownLine /> : <RiArrowDropRightLine />}</Button></h2>
+            <div className={`${styles.noteCollection} ${open? "" : styles.noteCollectionClosed}`}>
                 {notesList}
             </div>
         </div>
