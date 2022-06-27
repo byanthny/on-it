72f022a6d03dbed1d2c8b3ce62b93d46c9daa0d3
Anthony D <adas22@jhu.edu>
2022-06-27 16:52:48 -0400

    frontend: collection variants

--- projects/site/src/pages/notesPage.tsx
+++ projects/site/src/pages/notesPage.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import NoteCollection from "../components/items/Collection/Collection";
+import {Note as NoteModel} from "common";
+import Collection from "../components/items/Collection/Collection";
 import NavBar from "../components/navigation/NavBar/NavBar";
 import Header from "../components/navigation/Header/Header";
+import Note from "../components/items/Note/Note";
 
 const fakedata = [
   {
@@ -103,20 +105,34 @@ const fakedata = [
   },
 ];
 
-const notesPage = () => (
+interface ExtendedNoteModel extends NoteModel {
+  tags: Array<any>,
+  updatedAt: string
+}
+
+const notesPage = () => {
+
+  const renderNotes = (data:Array<ExtendedNoteModel>) =>
+    data.map(({ title, text, tags, updatedAt }) => (
+      <Note title={title} text={text} tags={tags} updatedAt={updatedAt} />
+    ));
+
+
+  return (
     <>
       <NavBar />
       <div className="main-content">
         <Header title="Notes" />
         <div className="secondary-content">
-          <NoteCollection collectionTitle="General" data={fakedata} />
-          <NoteCollection collectionTitle="General" data={fakedata} />
-          <NoteCollection collectionTitle="General" data={fakedata} />
-          <NoteCollection collectionTitle="General" data={fakedata} />
+          <Collection collectionTitle="General" variant="noteCollection">{renderNotes(fakedata)}</Collection>
+          <Collection collectionTitle="General" variant="noteCollection">{renderNotes(fakedata)}</Collection>
+          <Collection collectionTitle="General" variant="noteCollection">{renderNotes(fakedata)}</Collection>
+          <Collection collectionTitle="General" variant="noteCollection">{renderNotes(fakedata)}</Collection>
 
         </div>
       </div>
     </>
 );
+  }
 
 export default notesPage;
