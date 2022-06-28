f1738d7101ff71b0f253ef85da90c675a6f18025
Anthony D <adas22@jhu.edu>
2022-06-28 18:41:52 -0400

    frontend: helpers to regular exports and constants

---
+++ projects/site/src/utils/constants.tsx
@@ -0,0 +1,7 @@
+export const monthShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dev"];
+
+export const monthLong = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+export const millisecondsInADay = (1000 * 60 * 60 * 24);
