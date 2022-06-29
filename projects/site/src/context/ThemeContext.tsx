85b77f31bfc1d14beaddc1a46eb3a726dbeb75dd
Anthony D <adas22@jhu.edu>
2022-06-28 20:08:18 -0400

    frontend: useMemo in context

--- projects/site/src/context/ThemeContext.tsx
+++ projects/site/src/context/ThemeContext.tsx
@@ -1,16 +1,14 @@
-/* eslint react/jsx-no-constructed-context-values: 0 */
-
 import React, { createContext, useState } from "react";
 
-const intialTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark";
+const initialTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark";
 interface ThemeProviderProps {
   children: React.ReactNode;
 }
 
-export const ThemeContext = createContext<any>(intialTheme);
+export const ThemeContext = createContext<any>(initialTheme);
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState(intialTheme);
-  const value = { theme, setTheme };
+  const [theme, setTheme] = useState(initialTheme);
+  const value = React.useMemo(() => [theme, setTheme], [theme]);
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
 };
