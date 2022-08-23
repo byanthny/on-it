ce3a2ab42e1900bd290326f2c1b4c04f8d2da308
Anthony Das <adas22@jhu.edu>
2022-08-23 19:56:40 -0400

    frontend: useReducer for notes + map values

--- projects/site/src/pages/notesPage.tsx
+++ projects/site/src/pages/notesPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer, useState } from "react";
-import { Note as NoteModel } from "common";
+import { Note as NoteModel, Tag } from "common";
 import { toast } from "react-toastify";
 import OnItApi from "../services/OnItApi";
 import EditNote from "../components/items/EditNote";
@@ -20,34 +20,99 @@ const initialNote: NoteModel = {
   updated: "",
 };
 
+const tempTags = [
+  {
+    "_id": "testTag",
+    "uid": "string",
+    "name": "testTag",
+    "color": "#c5CC6D",
+    "created": "2022-08-23T23:13:06.983Z",
+    "updated": "2022-08-23T23:13:06.983Z"
+  },
+  {
+    "_id": "testTag",
+    "uid": "string",
+    "name": "",
+    "color": "#c5CC6D",
+    "created": "2022-08-23T23:13:06.983Z",
+    "updated": "2022-08-23T23:13:06.983Z"
+  },
+  {
+    "_id": "testTag2",
+    "uid": "string",
+    "name": "testTag2",
+    "color": "#c5CC6D",
+    "created": "2022-08-23T23:13:06.983Z",
+    "updated": "2022-08-23T23:13:06.983Z"
+  },
+  {
+    "_id": "testTag3",
+    "uid": "string",
+    "name": "testTag3",
+    "color": "#c5CC6D",
+    "created": "2022-08-23T23:13:06.983Z",
+    "updated": "2022-08-23T23:13:06.983Z"
+  }
+];
+
 type currentNoteData = {
   note: NoteModel;
   isEditing: boolean;
 };
 
+const reducer = (state: Map<any, any>, action: any) => {
+  switch (action.type) {
+    case "CREATE":
+      return state.set(action.payload.tag, action.payload.response);
+    case "UPDATE":
+      action.payload.response.tags.forEach((tag: Tag) => {
+        state.set(tag, state.get(tag).set(action.payload.id, action.payload.response));
+      });
+      return state;
+    case "DELETE":
+      action.payload.response.tags.forEach((tag: Tag) => {
+        state.set(tag, state.get(tag).delete(action.payload.id, action.payload.response));
+      });
+      return state;
+    default:
+      return state;
+  };
+}
+
 const notesPage = () => {
-  // TODO Use Reducer
-  const [noteData, setNoteData] = useState<Array<NoteModel>>([]);
-  // const [noteData, dispatch] = useReducer(reducer, []);
+  const [noteData, dispatch] = useReducer(reducer, new Map());
   const [modalOpen, setModalOpen] = useState(false);
   const [currentNote, setCurrentNote] = useState<currentNoteData>({ note: initialNote, isEditing: false });
 
   /* Fetch notes from api based on tags */
-  const fetchData = async () => {
+  const fetchData = async (tag: string) => {
     try {
-      const response = await OnItApi.note.search({});
-
+      const response = await OnItApi.note.search({ tags: [tag] });
+      console.log(tag);
       if (!response || response.error) throw response.error?.message;
 
-      setNoteData(response.payload!);
+      return response;
     } catch (error) {
       toast("Couldn't load notes :(" || (error as string));
     }
+    return null;
   };
+
+  const mapData = (fetch: Function) => {
+    tempTags.forEach((tag) => {
+      const response = fetch(tag);
+
+      if (!response)
+        console.log(`Couldn't load ${tag}`);
+      else
+        dispatch({ type: "CREATE", payload: { tag, response: response.payload } });
+
+    });
+  }
 
   // onMount load all notes
   useEffect(() => {
-    fetchData();
+    mapData(fetchData);
   }, []);
 
   // onClick select Note and set state to edit.
@@ -74,19 +139,46 @@ const notesPage = () => {
 
       setCurrentNote({ note: initialNote, isEditing: false });
 
-      // TODO Update Note Data
+      dispatch({ type: "UPDATE", payload: { id: currentNote.note._id, response: response.payload } });
+
     } catch (error) {
       toast("Error: Couldn't update note" || (error as string));
     }
   };
 
   /* Render Notes */
-  const renderNotes = (data: Array<NoteModel>) =>
-    data && data.length > 0
-      ? data.map((note) => (
-        <Note key={note._id} NoteData={note} selectNoteToEdit={selectNoteToEdit} />
-      ))
-      : null;
+  // const renderNotes = (data: Array<NoteModel>) =>
+  //   data && data.length > 0
+  //     ? data.map((note) => (
+  //       <Note key={note._id} NoteData={note} selectNoteToEdit={selectNoteToEdit} />
+  //     ))
+  //     : null;
+
+  const renderNotes = (data: Map<any, NoteModel>) => {
+    const toRender: Array<React.ReactNode> = [];
+    data.forEach((value) =>
+      toRender.push(<Note key={value._id} NoteData={value} selectNoteToEdit={selectNoteToEdit} />),
+    );
+    return toRender;
+  };
+
+  const renderNoteCollection = (data: any) => {
+    const toRender: Array<React.ReactNode> = [];
+    if (noteData) {
+      noteData.forEach((value, key) => {
+        if (key !== "none")
+          toRender.push(
+            <Collection collectionTitle={key} variant="normalCollection">
+              {renderNotes(value)}
+            </Collection>,
+          );
+        else toRender.push(renderNotes(value));
+      });
+    }
+    return toRender;
+  };
+
+  // TODO delete note
 
   return (
     <>
@@ -99,18 +191,7 @@ const notesPage = () => {
           <Modal open={currentNote.isEditing} onClose={updateNote} editNote>
             <EditNote noteData={currentNote.note} setNoteData={setCurrentNote} />
           </Modal>
-          <Collection collectionTitle="General" variant="noteCollection">
-            {renderNotes(fakedata)}
-          </Collection>
-          <Collection collectionTitle="General" variant="noteCollection">
-            {renderNotes(noteData)}
-          </Collection>
-          <Collection collectionTitle="General" variant="noteCollection">
-            {renderNotes(fakedata)}
-          </Collection>
-          <Collection collectionTitle="General" variant="noteCollection">
-            {renderNotes(fakedata)}
-          </Collection>
+          {renderNoteCollection(noteData)}
         </div>
       </div>
     </>
